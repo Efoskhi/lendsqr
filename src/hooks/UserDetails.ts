ab1deec1e85765c3817684f1b1c4e408d54b9b59
efoskhi <efosaosaheni@gmail.com>
2024-06-27 23:54:37 +0100

    Various Fixes

--- src/hooks/UserDetails.ts
+++ src/hooks/UserDetails.ts
@@ -1,19 +1,22 @@
 import React from "react";
 
-const UserDetails = (username) => {
+const UserDetails = (username : string) => {
     const [ isLoading, setIsLoading ] = React.useState(true);
     const [ userDetails, setUserDetails ] = React.useState<any>([]);
 
-    const fetchUserDetails = () => {
+    // const fetchUserDetails = () => {
+    //     const allUsers = JSON.parse(localStorage.getItem("allUsers") || '[]');
+    //     const user = allUsers.filter((item : any) => item.username === username);        
+    //     setUserDetails(user[0]);
+    //     setIsLoading(false)
+    // }
+
+    React.useEffect(() => {
         const allUsers = JSON.parse(localStorage.getItem("allUsers") || '[]');
         const user = allUsers.filter((item : any) => item.username === username);        
         setUserDetails(user[0]);
         setIsLoading(false)
-    }
-
-    React.useEffect(() => {
-        fetchUserDetails()
-    }, [fetchUserDetails])
+    }, [username])
 
    
 
